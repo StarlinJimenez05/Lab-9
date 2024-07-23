1130edfb9bc25334e00d337a53b778e92d045cb6
StarlinJ <starlinjimenez05@gmail.com>
2024-07-23 15:55:49 -0400

    Decode Function

--- main.py
+++ main.py
@@ -2,12 +2,25 @@ def encode(password):
     _list = []
     for i in password:
         i = int(i)
-        i = i +3
+        i = i + 3
         _list.append(i)
     s = ' '
-    result= s.join(_list)
+    result = s.join(_list)
     return result
 
-print(encode("12345555"))
+#print(encode("12345555"))
 
 
+def decode(password):
+    list = []
+    for i in password:
+        num = int(i)
+        num = num - 3
+        stringNum = str(num)
+        list.append(stringNum)
+    s = ''
+    string = s.join(list)
+    return string
+
+
+print(decode('45678888'))
